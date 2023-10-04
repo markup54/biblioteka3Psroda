e5277ada9196d936083f6ba287b49fab5e07f089
markup54 <marzenakupka@tlen.pl>
2023-10-04 13:18:25 +0200

    klasa osoba

---
+++ src/Osoba.java
@@ -0,0 +1,38 @@
+import java.util.Date;
+
+abstract public class Osoba {
+    private String imie;
+    private String nazwisko;
+
+    private Date dataUrodzenia;
+
+    public Osoba(String imie, String nazwisko,int rok,int m,int d) {
+        this.imie = imie;
+        this.nazwisko = nazwisko;
+        dataUrodzenia = new Date(rok,m,d);
+    }
+
+    public String getImie() {
+        return imie;
+    }
+
+    public void setImie(String imie) {
+        this.imie = imie;
+    }
+
+    public String getNazwisko() {
+        return nazwisko;
+    }
+
+    public void setNazwisko(String nazwisko) {
+        this.nazwisko = nazwisko;
+    }
+
+    public Date getDataUrodzenia() {
+        return dataUrodzenia;
+    }
+
+    public void setDataUrodzenia(Date dataUrodzenia) {
+        this.dataUrodzenia = dataUrodzenia;
+    }
+}
